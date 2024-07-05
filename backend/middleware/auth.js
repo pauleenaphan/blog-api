0edf0e5324763abe410469939b8b48c132a0151c
pauleena phan <pauleena2002@gmail.com>
2024-07-04 22:38:30 -0700

    added jwt middleware in separate file

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,21 +3,26 @@ require('dotenv').config();
 
 const authenticateJWT = (req, res, next) => {
     const authHeader = req.headers['authorization'];
+        
     if (!authHeader) {
-        return res.status(401).send('Access denied. No token provided.');
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
 
-    const token = authHeader.replace('Bearer ', '');
+    const token = authHeader.split(' ')[1]; // Extract token from "Bearer <token>"
+
     if (!token) {
-        return res.status(401).send('Access denied. No token provided.');
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
 
     try {
+        console.log("token ", token);
+        console.log("secret key: ", process.env.SECRET_KEY);
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
         req.user = decoded;
         next();
     } catch (error) {
-        res.status(400).send('Invalid token');
+        res.status(403).json({ message: 'Invalid token' }); // 403 for Forbidden, as token is invalid
     }
 };
 
